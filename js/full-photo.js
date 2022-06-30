821db36727f4c83b0050ef3c15b1974011af4aed
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-06-30 14:43:05 +0300

    выделила функцию отрисовки одного комментария, удалила классы, валидация хэштегов

--- js/full-photo.js
+++ js/full-photo.js
@@ -2,6 +2,7 @@ import { createElement, isEscapeKey } from './util.js';
 
 const WIDTH = 35;
 const HEIGHT = 35;
+const PART_OF_COMMENTS = 5;
 
 const closeButton = document.querySelector('.big-picture__cancel');
 const bigPicture = document.querySelector('.big-picture');
@@ -10,13 +11,14 @@ const likesCount = bigPicture.querySelector('.likes-count');
 const commentsCount = bigPicture.querySelector('.comments-count');
 const socialComments = bigPicture.querySelector('.social__comments');
 const pictureDescription = bigPicture.querySelector('.social__caption');
-const body = document.querySelector('body');
+const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+const socialCommentsLoader = bigPicture.querySelector('.social__comments-loader');
 
 const similarUserFragment = document.createDocumentFragment();
 
 const closeModal = () => {
   bigPicture.classList.add('hidden');
-  document.querySelector('body').classList.remove('modal-open');
+  document.body.classList.remove('modal-open');
   removeEventListener();
 };
 
@@ -37,7 +39,23 @@ function removeEventListener() {
   closeButton.removeEventListener('click', onCloseButtonClick);
 }
 
+const renderOneComment = (comment) => {
+  const userComment = createElement('li', 'social__comment');
+  const userCommentImage = createElement('img', 'social__picture');
+  const userCommentText = createElement('p', 'social__text');
+  userCommentImage.src = comment.avatar;
+  userCommentImage.alt = comment.name;
+  userCommentImage.style.width = WIDTH;
+  userCommentImage.style.height = HEIGHT;
+  userComment.append(userCommentImage);
+  userCommentText.textContent = comment.messages;
+  userComment.append(userCommentText);
+  similarUserFragment.append(userComment);
+};
+
+
 const showBigPicture = (picture) => {
+
 
   bigPictureImage.src = picture.url;
   bigPictureImage.alt = picture.description;
@@ -45,26 +63,16 @@ const showBigPicture = (picture) => {
   commentsCount.textContent = picture.comments.length;
 
   picture.comments.forEach((comment) => {
-    const userComment = createElement('li', 'social__comment');
-    const userCommentImage = createElement('img', 'social__picture');
-    const userCommentText = createElement('p', 'social__text');
-    userCommentImage.src = comment.avatar;
-    userCommentImage.alt = comment.name;
-    userCommentImage.style.width = WIDTH;
-    userCommentImage.style.height = HEIGHT;
-    userComment.append(userCommentImage);
-    userCommentText.textContent =comment.messages;
-    userComment.append(userCommentText);
-    similarUserFragment.append(userComment);
+    renderOneComment(comment);
   });
+
 
   socialComments.innerHTML = '';
   socialComments.append(similarUserFragment);
   pictureDescription.textContent = picture.description;
   bigPicture.classList.remove('hidden');
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
-  body.classList.add('modal-open');
+  socialCommentCount.classList.remove('hidden');
+  document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
   closeButton.addEventListener('click', onCloseButtonClick);
 };
