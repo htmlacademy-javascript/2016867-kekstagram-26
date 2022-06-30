901c1afb51c9b29d5dab81f2ffe0c02c9dc6d644
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-06-30 20:35:39 +0300

    кнопка "загрузить фото"

--- js/full-photo.js
+++ js/full-photo.js
@@ -10,6 +10,7 @@ const bigPictureImage = document.querySelector('.big-picture__img img');
 const likesCount = bigPicture.querySelector('.likes-count');
 const commentsCount = bigPicture.querySelector('.comments-count');
 const socialComments = bigPicture.querySelector('.social__comments');
+const oneSocialComment = bigPicture.querySelector('.social__comment');
 const pictureDescription = bigPicture.querySelector('.social__caption');
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const socialCommentsLoader = bigPicture.querySelector('.social__comments-loader');
@@ -55,26 +56,63 @@ const renderOneComment = (comment) => {
 
 
 const showBigPicture = (picture) => {
-
+  let countClickLoadComments = 0;
+  bigPicture.classList.remove('hidden');
+  socialCommentCount.classList.remove('hidden');
 
   bigPictureImage.src = picture.url;
   bigPictureImage.alt = picture.description;
   likesCount.textContent = picture.likes;
+
   commentsCount.textContent = picture.comments.length;
+  const allCommentsCount = picture.comments.length;
+  socialComments.textContent = '';
 
-  picture.comments.forEach((comment) => {
-    renderOneComment(comment);
-  });
+  const commentsPartsCount = Math.ceil(allCommentsCount / PART_OF_COMMENTS);
+  /*почему при попытке записать тернарным оператором выпадает ошибка?
+  PART_OF_COMMENTS >= allCommentsCount ? socialCommentsLoader.classList.add('hidden') : socialCommentsLoader.classList.remove('hidden'); */
+  if (PART_OF_COMMENTS >= allCommentsCount) {
+    socialCommentsLoader.classList.add('hidden');
+  }
+  else {
+    socialCommentsLoader.classList.remove('hidden');
+  }
 
+  const addCommentsToList = () => {
+    picture.comments.forEach((comment) => {
+      renderOneComment(comment);
+      // picture.comments.every(oneSocialComment.classList.add('hidden'));
+    });
+    /*Идея заключалась в том, чтобы сформировать список комментариев, повесить каждому класс hidden, а затем партиями удалять этот класс */
+
+    const shownComments = (countClickLoadComments + 1) * PART_OF_COMMENTS <= allCommentsCount ? (countClickLoadComments + 1) * PART_OF_COMMENTS : allCommentsCount;
+    socialCommentCount.textContent = `${shownComments} из ${allCommentsCount} комментариев`;
+    /* for (let i = shownComments-5; i< shownComments; i++) {
+      picture.comments.every(oneSocialComment.classList.remove('hidden'));
+    }*/
+  };
+
+  addCommentsToList();
 
   socialComments.innerHTML = '';
   socialComments.append(similarUserFragment);
   pictureDescription.textContent = picture.description;
-  bigPicture.classList.remove('hidden');
-  socialCommentCount.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
   closeButton.addEventListener('click', onCloseButtonClick);
+  /*почему так: по клику изменяются данные, поэтому вынести их куда-то не получилось, оставила внутри. Но в связи с этим при попытке удалить обработчик и сбросить данные
+  при нажатии на крестик (чтобы он обнулял счетчик кликов) не получилось вынести в функцию удаления обработчиков  removeEventListener().
+  JS CВОДИТ МЕНЯ С УМА!!!!!!!*/
+  socialCommentsLoader.addEventListener('click', () => {
+    countClickLoadComments++;
+    if (countClickLoadComments === commentsPartsCount - 1) {
+      socialCommentsLoader.classList.add('hidden');
+      addCommentsToList();
+    }
+    else {
+      addCommentsToList();
+    }
+  });
 };
 
 export {showBigPicture};
