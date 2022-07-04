a5444fcaca39d8a4f70505592f492f6ae9adce29
OlgaNovichikhina <97948178+OlgaNovichikhina@users.noreply.github.com>
2022-07-04 19:13:32 +0300

    добавление эффектов для фото

--- js/upload-form.js
+++ js/upload-form.js
@@ -7,6 +7,34 @@ const PERCENT_STEP = 25;
 let percent = 100;
 let scale = 1;
 
+const RANGE_OPTIONS = {
+  'grayscale': {
+    min: 0,
+    max: 1,
+    step: 0.1
+  },
+  'sepia': {
+    min: 0,
+    max: 1,
+    step: 0.1
+  },
+  'invert': {
+    min: 0,
+    max: 100,
+    step: 1
+  },
+  'blur': {
+    min: 0,
+    max: 3,
+    step: 0.1
+  },
+  'brightness': {
+    min: 1,
+    max: 3,
+    step: 0.1
+  },
+};
+
 const uploadFile = document.querySelector('#upload-file');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const uploadCancel = document.querySelector('#upload-cancel');
@@ -18,6 +46,24 @@ const scaleControlSmaller = document.querySelector('.scale__control--smaller');
 const scaleControlBigger = document.querySelector('.scale__control--bigger');
 const scaleControlValue = document.querySelector('.scale__control--value');
 const uploadPicture = document.querySelector('.img-upload__preview img');
+
+const imagePreview = document.querySelector('.img-upload__preview img');
+const effectsListElement = document.querySelector('.effects__list');
+const effectLevelFieldset = document.querySelector('.effect-level');
+const effectValueElement = document.querySelector('.effect-level__value');
+
+const FILTER_NAME = {
+  'chrome': 'grayscale',
+  'sepia': 'sepia',
+  'marvin': 'invert',
+  'phobos': 'blur',
+  'heat': 'brightness'
+};
+
+const UNIT = {
+  'invert': '%',
+  'blur': 'px',
+};
 
 const closeModal = () => {
   imgUploadOverlay.classList.add('hidden');
@@ -100,11 +146,64 @@ const onControlBiggerClick = (evt)  =>{
   scale = scale + SCALE_STEP;
 };
 
+const createSlider = () => {
+  noUiSlider.create(effectLevelFieldset, {
+    range: {
+      min: 0,
+      max: 100
+    },
+    start: 100,
+    step: 1,
+    connect: 'lower',
+  });
+};
+
+const changeEffect = (effectValue) => {
+  if(effectValue === 'none') {
+    effectLevelFieldset.noUiSlider.destroy();
+    imagePreview.style = '';
+    imagePreview.className = '';
+    return;
+  }
+
+  if (!effectLevelFieldset.noUiSlider) {
+    createSlider();
+  }
+
+  const effect = FILTER_NAME[effectValue];
+  const { min, max, step } = RANGE_OPTIONS[effect];
+  const unit = UNIT[effect]?UNIT[effect]:'';
+
+  imagePreview.className = '';
+  imagePreview.classList.add(`effects__preview--${effectValue}`);
+
+  effectLevelFieldset.noUiSlider.updateOptions({
+    range: {
+      min,
+      max,
+    },
+    start: max,
+    step,
+    connect: 'lower',
+  });
+
+  effectLevelFieldset.noUiSlider.on('update', () => {
+    effectValueElement.value = effectLevelFieldset.noUiSlider.get();
+    imagePreview.style.filter =`${effect}(${effectValueElement.value}${unit})`;
+  });
+};
+
+const onSliderChange = (evt) =>{
+  evt.preventDefault();
+  changeEffect(evt.target.value);
+};
+
 function addEventListeners() {
   document.addEventListener('keydown', onDocumentKeydown);
   uploadCancel.addEventListener('click',  onUploadCancelClick);
   scaleControlSmaller.addEventListener('click', onControlSmallerClick);
   scaleControlBigger.addEventListener('click', onControlBiggerClick);
+  effectsListElement.addEventListener('change', onSliderChange);
 }
 
 function removeEventListeners() {
@@ -112,6 +211,7 @@ function removeEventListeners() {
   uploadCancel.removeEventListener('click', onUploadCancelClick);
   scaleControlSmaller.removeEventListener('click', onControlSmallerClick);
   scaleControlBigger.removeEventListener('click', onControlBiggerClick);
+  effectsListElement.removeEventListener('change', onSliderChange);
 }
 
 const initUploadFormAction = () => {
